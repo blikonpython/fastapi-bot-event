d4535e87918dd76fa86448c3ff5880500fd0a08d
msedeno23 <msedeno.blikon@gmail.com>
2024-08-01 09:56:07 -0600

    Refactor FirebaseConfig to use environment variables.
    Moved Firebase credentials loading to use environment variables instead of a static file. This enhances security and flexibility, allowing credentials to be managed more securely.

--- app/core/config.py
+++ app/core/config.py
@@ -1,10 +1,14 @@
+import os
+import json
 import firebase_admin
 from firebase_admin import credentials, firestore
 
 class FirebaseConfig:
     def __init__(self):
-        cred = credentials.Certificate("app/firebase_credentials.json")
-        firebase_admin.initialize_app(cred)
-        self.db = firestore.client()
+        cred_info = os.getenv("FIREBASE_CREDENTIALS")
+        if cred_info:
+            cred = credentials.Certificate(json.loads(cred_info))
+            firebase_admin.initialize_app(cred)
+            self.db = firestore.client()
 
 firebase_config = FirebaseConfig()
